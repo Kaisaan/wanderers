4c8d8016eba58bef68e8234709ba8a29b5b4d0bb
Cameron Schmidt <4950661+cschmidt0121@users.noreply.github.com>
2026-05-21 22:52:21 -0400

    To CSV script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Repo for translating Ys III: Wanderers From Ys on PS2
 # Notes
 - Game uses the same graphics format as Lost Kefin (NAXA5010 header)
 - Some graphics have a GBXA2000 header which is just like the other format without any of the frame info (so single, static images)
-- Uses the same kscript format as Lost Kefin except pointer space is 0x800 bytes large
+- Uses the same script container format as Lost Kefin except pointer space is 0x800 bytes large. Uses different opcodes.
 - Shift-JIS values seem to be XOR encrypted? (look at function at 00174A8C)
 - DATA.BIN can be extract the same way as Lost Kefin
 
---
+++ scripts/tasks/to_csv.py
@@ -0,0 +1,65 @@
+import csv
+import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+
+from libwanderers.script import line_to_op
+
+
+def to_csv(wscript_file, csv_file):
+    """
+    Pull out just the textual operations from a wscript file and save to CSV
+    """
+    base_filename = os.path.basename(wscript_file)
+    wscript_fp = open(wscript_file, "r", encoding="utf-8")
+    csv_fp = open(csv_file, "w", newline="", encoding="utf-8")
+    writer = csv.writer(csv_fp)
+    writer.writerow(
+        ["ID", "Block", "Speaker", "JP Text", "EN Text", "Comments", "Text Type"]
+    )
+
+    i = 0
+    block_index = 0
+
+    for line in wscript_fp.readlines():
+        line = line.lstrip().rstrip("\n")
+        # Using labels, we can roughly split script files into "blocks"
+        # which may make it easier for translators to logically break the
+        # script up.
+        if "LABEL_" in line:
+            block_index += 1
+            continue
+        if "JMP_" in line:
+            continue
+
+        op = line_to_op(line)
+        op_type = op.__class__.__name__
+
+        if op_type in [
+            "VNText",
+            "CutsceneText",
+        ]:
+            text = op.to_object()["text"]
+            text = text.replace("\\n", "\n")
+            speaker = op.to_object().get("character_name", "").replace("*", "")
+            writer.writerow(
+                [
+                    f"{base_filename}||{i}||{0}",
+                    block_index,
+                    speaker,
+                    text,
+                    "",
+                    "",
+                    op_type,
+                ]
+            )
+        i += 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python scripts/tasks/to_csv.py <wscript_file> <csv_file>")
+        sys.exit(1)
+    to_csv(sys.argv[1], sys.argv[2])
