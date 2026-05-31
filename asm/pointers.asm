a4332e4bc244e998d5fff82e6ea3e4939cce0d41
Kaisaan <34224128+Kaisaan@users.noreply.github.com>
2026-05-30 23:48:37 -0400

    Add strings

--- asm/pointers.asm
+++ asm/pointers.asm
@@ -1,3 +1,5 @@
+.loadtable "asm/wanderers.tbl", "UTF8"
+
 .include "asm/strings.asm", "UTF8"
 
 // Offset equ 0xFFF80
@@ -35,55 +37,55 @@
 
 .orga 0x15AC90
 
-@L15AC90: .asciiz L15AC90
-@L15ACA8: .asciiz L15ACA8
-@L15ACB0: .asciiz L15ACB0
-@L15ACC8: .asciiz L15ACC8
-@L15ACD8: .asciiz L15ACD8
-@L15ACE8: .asciiz L15ACE8
-@L15ACF8: .asciiz L15ACF8
-@L15AD08: .asciiz L15AD08
-@L15AD18: .asciiz L15AD18
-@L15AD30: .asciiz L15AD30
-@L15AD48: .asciiz L15AD48
-@L15AD58: .asciiz L15AD58
-@L15AD68: .asciiz L15AD68
-@L15AD78: .asciiz L15AD78
-@L15AD88: .asciiz L15AD88
-@L15ADA0: .asciiz L15ADA0
-@L15ADB8: .asciiz L15ADB8
-@L15ADC8: .asciiz L15ADC8
-@L15ADD8: .asciiz L15ADD8
-@L15ADE8: .asciiz L15ADE8
-@L15ADF0: .asciiz L15ADF0
-@L15AE00: .asciiz L15AE00
-@L15AE10: .asciiz L15AE10
-@L15AE20: .asciiz L15AE20
+@L15AC90: .str L15AC90
+@L15ACA8: .str L15ACA8
+@L15ACB0: .str L15ACB0
+@L15ACC8: .str L15ACC8
+@L15ACD8: .str L15ACD8
+@L15ACE8: .str L15ACE8
+@L15ACF8: .str L15ACF8
+@L15AD08: .str L15AD08
+@L15AD18: .str L15AD18
+@L15AD30: .str L15AD30
+@L15AD48: .str L15AD48
+@L15AD58: .str L15AD58
+@L15AD68: .str L15AD68
+@L15AD78: .str L15AD78
+@L15AD88: .str L15AD88
+@L15ADA0: .str L15ADA0
+@L15ADB8: .str L15ADB8
+@L15ADC8: .str L15ADC8
+@L15ADD8: .str L15ADD8
+@L15ADE8: .str L15ADE8
+@L15ADF0: .str L15ADF0
+@L15AE00: .str L15AE00
+@L15AE10: .str L15AE10
+@L15AE20: .str L15AE20
 
 // Character Names
 
 .orga 0x14DFA8
 
-@L14DFA8: .asciiz L14DFA8
-@L14DFB0: .asciiz L14DFB0
-@L14DFB8: .asciiz L14DFB8
-@L14DFC8: .asciiz L14DFC8
-@L14DFD0: .asciiz L14DFD0
-@L14DFE0: .asciiz L14DFE0
-@L14DFE8: .asciiz L14DFE8
-@L14DFF0: .asciiz L14DFF0
-@L14E000: .asciiz L14E000
-@L14E010: .asciiz L14E010
-@L14E018: .asciiz L14E018
-@L14E028: .asciiz L14E028
-@L14E038: .asciiz L14E038
-@L14E048: .asciiz L14E048
-@L14E058: .asciiz L14E058
-@L14E060: .asciiz L14E060
-@L14E068: .asciiz L14E068
-@L14E078: .asciiz L14E078
-@L14E080: .asciiz L14E080
-@L14E088: .asciiz L14E088
+@L14DFA8: .str L14DFA8
+@L14DFB0: .str L14DFB0
+@L14DFB8: .str L14DFB8
+@L14DFC8: .str L14DFC8
+@L14DFD0: .str L14DFD0
+@L14DFE0: .str L14DFE0
+@L14DFE8: .str L14DFE8
+@L14DFF0: .str L14DFF0
+@L14E000: .str L14E000
+@L14E010: .str L14E010
+@L14E018: .str L14E018
+@L14E028: .str L14E028
+@L14E038: .str L14E038
+@L14E048: .str L14E048
+@L14E058: .str L14E058
+@L14E060: .str L14E060
+@L14E068: .str L14E068
+@L14E078: .str L14E078
+@L14E080: .str L14E080
+@L14E088: .str L14E088
 
 // Character Name pointers
 
@@ -122,3 +124,417 @@
 .dw @L14DFE0
 .dw @L14E088
 
+// Save Filename?
+
+.orga 0x14E2B0 :: @L14E2B0 : .str L14E2B0
+.orga 0x15F1D8 :: .dw @L14E2B0
+
+// Area Name pointers
+
+.orga 0x134890
+
+.dw @L14E340
+.dw @L14E350
+.dw @L14E370
+.dw @L14E370
+.dw @L14E350
+.dw @L14E388
+.dw @L14E398
+.dw @L14E398
+.dw @L14E3A8
+.dw @L14E3B8
+.dw @L14E3C8
+.dw @L14E3D8
+.dw @L14E3E8
+.dw @L14E3F8
+.dw @L14E408
+.dw @L14E418
+.dw @L14E428
+.dw @L14E438
+.dw @L14E448
+.dw @L14E458
+.dw @L14E468
+.skip 4         // Difficulty options
+.dw @L14E478
+.dw @L14E480
+.dw @L14E488
+
+// Area Name Text
+
+.orga 0x14E340
+
+@L14E340: .str L14E340
+@L14E350: .str L14E350
+@L14E370: .str L14E370
+@L14E388: .str L14E388
+@L14E398: .str L14E398
+@L14E3A8: .str L14E3A8
+@L14E3B8: .str L14E3B8
+@L14E3C8: .str L14E3C8
+@L14E3D8: .str L14E3D8
+@L14E3E8: .str L14E3E8
+@L14E3F8: .str L14E3F8
+@L14E408: .str L14E408
+@L14E418: .str L14E418
+@L14E428: .str L14E428
+@L14E438: .str L14E438
+@L14E448: .str L14E448
+@L14E458: .str L14E458
+@L14E468: .str L14E468
+
+// Difficulty options
+
+@L14E478: .str L14E478
+@L14E480: .str L14E480
+@L14E488: .str L14E488
+
+// Hardcoded stuff (I cant find pointers)
+
+.orga 0x14E490 :: @L14E490: .str L14E490
+.orga 0x14E4A0 :: @L14E4A0: .str L14E4A0
+.orga 0x14E4A8 :: @L14E4A8: .str L14E4A8
+.orga 0x14E4B0 :: @L14E4B0: .str L14E4B0
+.orga 0x14E4C0 :: @L14E4C0: .str L14E4C0
+.orga 0x14E4F0 :: @L14E4F0: .str L14E4F0
+.orga 0x14E520 :: @L14E520: .str L14E520
+.orga 0x14E560 :: @L14E560: .str L14E560
+.orga 0x14E5C0 :: @L14E5C0: .str L14E5C0
+.orga 0x14E5F0 :: @L14E5F0: .str L14E5F0
+.orga 0x14E6A0 :: @L14E6A0: .str L14E6A0
+.orga 0x14E6C0 :: @L14E6C0: .str L14E6C0
+.orga 0x14E6F0 :: @L14E6F0: .str L14E6F0
+.orga 0x14E780 :: @L14E780: .str L14E780
+.orga 0x14E7A0 :: @L14E7A0: .str L14E7A0
+.orga 0x14E7D0 :: @L14E7D0: .str L14E7D0
+.orga 0x14E7F0 :: @L14E7F0: .str L14E7F0
+.orga 0x14E880 :: @L14E880: .str L14E880
+.orga 0x14E900 :: @L14E900: .str L14E900
+.orga 0x14E9A0 :: @L14E9A0: .str L14E9A0
+.orga 0x14E9D0 :: @L14E9D0: .str L14E9D0
+.orga 0x14EA70 :: @L14EA70: .str L14EA70
+.orga 0x14EAC0 :: @L14EAC0: .str L14EAC0
+.orga 0x14EBF0 :: @L14EBF0: .str L14EBF0
+.orga 0x14ECA0 :: @L14ECA0: .str L14ECA0
+.orga 0x14EDA0 :: @L14EDA0: .str L14EDA0
+.orga 0x14EDD0 :: @L14EDD0: .str L14EDD0
+.orga 0x14EE00 :: @L14EE00: .str L14EE00
+.orga 0x14EE30 :: @L14EE30: .str L14EE30
+.orga 0x14EE50 :: @L14EE50: .str L14EE50
+.orga 0x14EE78 :: @L14EE78: .str L14EE78
+.orga 0x14EE80 :: @L14EE80: .str L14EE80
+.orga 0x14EE88 :: @L14EE88: .str L14EE88
+.orga 0x14EEA0 :: @L14EEA0: .str L14EEA0
+.orga 0x14F1A0 :: @L14F1A0: .str L14F1A0
+.orga 0x14F1E0 :: @L14F1E0: .str L14F1E0
+.orga 0x14F250 :: @L14F250: .str L14F250
+.orga 0x14F2C0 :: @L14F2C0: .str L14F2C0
+.orga 0x14F320 :: @L14F320: .str L14F320
+.orga 0x14F550 :: @L14F550: .str L14F550
+.orga 0x14F558 :: @L14F558: .str L14F558
+.orga 0x14F560 :: @L14F560: .str L14F560
+.orga 0x14F568 :: @L14F568: .str L14F568
+.orga 0x14F570 :: @L14F570: .str L14F570
+.orga 0x14F578 :: @L14F578: .str L14F578
+.orga 0x14F5A8 :: @L14F5A8: .str L14F5A8
+.orga 0x14F5B0 :: @L14F5B0: .str L14F5B0
+.orga 0x14F5C0 :: @L14F5C0: .str L14F5C0
+.orga 0x14F5E0 :: @L14F5E0: .str L14F5E0
+.orga 0x14F600 :: @L14F600: .str L14F600
+.orga 0x14F620 :: @L14F620: .str L14F620
+.orga 0x14F640 :: @L14F640: .str L14F640
+.orga 0x14F660 :: @L14F660: .str L14F660
+.orga 0x14F680 :: @L14F680: .str L14F680
+.orga 0x14F6A0 :: @L14F6A0: .str L14F6A0
+.orga 0x14F6B8 :: @L14F6B8: .str L14F6B8
+.orga 0x14F6C0 :: @L14F6C0: .str L14F6C0
+.orga 0x14F6D0 :: @L14F6D0: .str L14F6D0
+.orga 0x14F6D8 :: @L14F6D8: .str L14F6D8
+.orga 0x14F6E0 :: @L14F6E0: .str L14F6E0
+.orga 0x14F6E8 :: @L14F6E8: .str L14F6E8
+.orga 0x14F6F0 :: @L14F6F0: .str L14F6F0
+.orga 0x14F6F8 :: @L14F6F8: .str L14F6F8
+.orga 0x14F740 :: @L14F740: .str L14F740
+.orga 0x14F760 :: @L14F760: .str L14F760
+.orga 0x14F7C0 :: @L14F7C0: .str L14F7C0
+.orga 0x14F820 :: @L14F820: .str L14F820
+.orga 0x14F880 :: @L14F880: .str L14F880
+.orga 0x14F8E0 :: @L14F8E0: .str L14F8E0
+.orga 0x14F950 :: @L14F950: .str L14F950
+.orga 0x14F970 :: @L14F970: .str L14F970
+.orga 0x14F9C0 :: @L14F9C0: .str L14F9C0
+.orga 0x14FA30 :: @L14FA30: .str L14FA30
+.orga 0x14FAA0 :: @L14FAA0: .str L14FAA0
+.orga 0x14FB10 :: @L14FB10: .str L14FB10
+.orga 0x14FB80 :: @L14FB80: .str L14FB80
+.orga 0x14FBF0 :: @L14FBF0: .str L14FBF0
+.orga 0x14FC60 :: @L14FC60: .str L14FC60
+.orga 0x14FCD0 :: @L14FCD0: .str L14FCD0
+.orga 0x14FD40 :: @L14FD40: .str L14FD40
+.orga 0x14FDB0 :: @L14FDB0: .str L14FDB0
+.orga 0x14FE00 :: @L14FE00: .str L14FE00
+.orga 0x14FE70 :: @L14FE70: .str L14FE70
+.orga 0x14FEE0 :: @L14FEE0: .str L14FEE0
+.orga 0x14FF50 :: @L14FF50: .str L14FF50
+.orga 0x14FFB0 :: @L14FFB0: .str L14FFB0
+.orga 0x150020 :: @L150020: .str L150020
+.orga 0x150080 :: @L150080: .str L150080
+.orga 0x1500F0 :: @L1500F0: .str L1500F0
+.orga 0x150160 :: @L150160: .str L150160
+.orga 0x1501D0 :: @L1501D0: .str L1501D0
+.orga 0x150240 :: @L150240: .str L150240
+.orga 0x1502B0 :: @L1502B0: .str L1502B0
+.orga 0x150310 :: @L150310: .str L150310
+.orga 0x150380 :: @L150380: .str L150380
+.orga 0x1503F0 :: @L1503F0: .str L1503F0
+.orga 0x150450 :: @L150450: .str L150450
+.orga 0x1504B0 :: @L1504B0: .str L1504B0
+.orga 0x150520 :: @L150520: .str L150520
+.orga 0x150590 :: @L150590: .str L150590
+.orga 0x1505D0 :: @L1505D0: .str L1505D0
+.orga 0x150620 :: @L150620: .str L150620
+.orga 0x150660 :: @L150660: .str L150660
+.orga 0x1506B0 :: @L1506B0: .str L1506B0
+.orga 0x1506F0 :: @L1506F0: .str L1506F0
+.orga 0x150740 :: @L150740: .str L150740
+.orga 0x1507A0 :: @L1507A0: .str L1507A0
+.orga 0x1507F0 :: @L1507F0: .str L1507F0
+.orga 0x150840 :: @L150840: .str L150840
+.orga 0x1508A0 :: @L1508A0: .str L1508A0
+.orga 0x1508E0 :: @L1508E0: .str L1508E0
+.orga 0x150930 :: @L150930: .str L150930
+.orga 0x150980 :: @L150980: .str L150980
+.orga 0x1509F0 :: @L1509F0: .str L1509F0
+.orga 0x150A20 :: @L150A20: .str L150A20
+.orga 0x150A80 :: @L150A80: .str L150A80
+.orga 0x150A90 :: @L150A90: .str L150A90
+.orga 0x150AA0 :: @L150AA0: .str L150AA0
+.orga 0x150AB0 :: @L150AB0: .str L150AB0
+.orga 0x150AC0 :: @L150AC0: .str L150AC0
+.orga 0x150AD8 :: @L150AD8: .str L150AD8
+.orga 0x150AE8 :: @L150AE8: .str L150AE8
+.orga 0x150AF8 :: @L150AF8: .str L150AF8
+.orga 0x150B10 :: @L150B10: .str L150B10
+.orga 0x150B28 :: @L150B28: .str L150B28
+.orga 0x150B38 :: @L150B38: .str L150B38
+.orga 0x150B50 :: @L150B50: .str L150B50
+.orga 0x150B68 :: @L150B68: .str L150B68
+.orga 0x150B78 :: @L150B78: .str L150B78
+.orga 0x150B88 :: @L150B88: .str L150B88
+.orga 0x150B98 :: @L150B98: .str L150B98
+.orga 0x150BA8 :: @L150BA8: .str L150BA8
+.orga 0x150BB8 :: @L150BB8: .str L150BB8
+.orga 0x150BC8 :: @L150BC8: .str L150BC8
+.orga 0x150BD8 :: @L150BD8: .str L150BD8
+.orga 0x150BE8 :: @L150BE8: .str L150BE8
+.orga 0x150BF0 :: @L150BF0: .str L150BF0
+.orga 0x150C00 :: @L150C00: .str L150C00
+.orga 0x150C10 :: @L150C10: .str L150C10
+.orga 0x150C20 :: @L150C20: .str L150C20
+.orga 0x150C30 :: @L150C30: .str L150C30
+.orga 0x150C40 :: @L150C40: .str L150C40
+.orga 0x150C50 :: @L150C50: .str L150C50
+.orga 0x150C60 :: @L150C60: .str L150C60
+.orga 0x150C70 :: @L150C70: .str L150C70
+.orga 0x150C80 :: @L150C80: .str L150C80
+.orga 0x150C98 :: @L150C98: .str L150C98
+.orga 0x150CA8 :: @L150CA8: .str L150CA8
+.orga 0x150CB8 :: @L150CB8: .str L150CB8
+.orga 0x150CC8 :: @L150CC8: .str L150CC8
+.orga 0x150CD8 :: @L150CD8: .str L150CD8
+.orga 0x150CE8 :: @L150CE8: .str L150CE8
+.orga 0x150CF8 :: @L150CF8: .str L150CF8
+.orga 0x150D08 :: @L150D08: .str L150D08
+.orga 0x150D18 :: @L150D18: .str L150D18
+.orga 0x150D28 :: @L150D28: .str L150D28
+.orga 0x150D38 :: @L150D38: .str L150D38
+.orga 0x150D48 :: @L150D48: .str L150D48
+.orga 0x150D58 :: @L150D58: .str L150D58
+.orga 0x150D68 :: @L150D68: .str L150D68
+.orga 0x150D78 :: @L150D78: .str L150D78
+.orga 0x150D80 :: @L150D80: .str L150D80
+.orga 0x150D90 :: @L150D90: .str L150D90
+.orga 0x150DB0 :: @L150DB0: .str L150DB0
+.orga 0x150DD0 :: @L150DD0: .str L150DD0
+.orga 0x150E20 :: @L150E20: .str L150E20
+.orga 0x150E50 :: @L150E50: .str L150E50
+.orga 0x150E70 :: @L150E70: .str L150E70
+.orga 0x150E90 :: @L150E90: .str L150E90
+.orga 0x150EE0 :: @L150EE0: .str L150EE0
+.orga 0x150F00 :: @L150F00: .str L150F00
+.orga 0x150F18 :: @L150F18: .str L150F18
+.orga 0x150F30 :: @L150F30: .str L150F30
+.orga 0x150F60 :: @L150F60: .str L150F60
+.orga 0x150FB0 :: @L150FB0: .str L150FB0
+.orga 0x151000 :: @L151000: .str L151000
+.orga 0x151020 :: @L151020: .str L151020
+.orga 0x151040 :: @L151040: .str L151040
+.orga 0x151060 :: @L151060: .str L151060
+.orga 0x151080 :: @L151080: .str L151080
+.orga 0x1510A0 :: @L1510A0: .str L1510A0
+.orga 0x1510F0 :: @L1510F0: .str L1510F0
+.orga 0x151140 :: @L151140: .str L151140
+.orga 0x151190 :: @L151190: .str L151190
+.orga 0x1511A0 :: @L1511A0: .str L1511A0
+.orga 0x1511C0 :: @L1511C0: .str L1511C0
+.orga 0x1511D8 :: @L1511D8: .str L1511D8
+.orga 0x1511E8 :: @L1511E8: .str L1511E8
+.orga 0x151200 :: @L151200: .str L151200
+.orga 0x151218 :: @L151218: .str L151218
+.orga 0x151230 :: @L151230: .str L151230
+.orga 0x151240 :: @L151240: .str L151240
+.orga 0x151248 :: @L151248: .str L151248
+.orga 0x151258 :: @L151258: .str L151258
+.orga 0x151260 :: @L151260: .str L151260
+.orga 0x151270 :: @L151270: .str L151270
+.orga 0x151278 :: @L151278: .str L151278
+.orga 0x151288 :: @L151288: .str L151288
+.orga 0x151298 :: @L151298: .str L151298
+.orga 0x1512A8 :: @L1512A8: .str L1512A8
+.orga 0x1512B8 :: @L1512B8: .str L1512B8
+.orga 0x1512C8 :: @L1512C8: .str L1512C8
+.orga 0x1512D8 :: @L1512D8: .str L1512D8
+.orga 0x1512E8 :: @L1512E8: .str L1512E8
+.orga 0x1512F8 :: @L1512F8: .str L1512F8
+.orga 0x151308 :: @L151308: .str L151308
+.orga 0x151310 :: @L151310: .str L151310
+.orga 0x151330 :: @L151330: .str L151330
+.orga 0x151340 :: @L151340: .str L151340
+.orga 0x151360 :: @L151360: .str L151360
+.orga 0x151370 :: @L151370: .str L151370
+.orga 0x151380 :: @L151380: .str L151380
+.orga 0x151390 :: @L151390: .str L151390
+.orga 0x1513A0 :: @L1513A0: .str L1513A0
+.orga 0x1513B0 :: @L1513B0: .str L1513B0
+.orga 0x1513C0 :: @L1513C0: .str L1513C0
+.orga 0x1513C8 :: @L1513C8: .str L1513C8
+.orga 0x1513D8 :: @L1513D8: .str L1513D8
+.orga 0x1513E8 :: @L1513E8: .str L1513E8
+.orga 0x1513F8 :: @L1513F8: .str L1513F8
+.orga 0x151408 :: @L151408: .str L151408
+.orga 0x151418 :: @L151418: .str L151418
+.orga 0x151428 :: @L151428: .str L151428
+.orga 0x151438 :: @L151438: .str L151438
+.orga 0x151448 :: @L151448: .str L151448
+.orga 0x151458 :: @L151458: .str L151458
+.orga 0x151460 :: @L151460: .str L151460
+.orga 0x151470 :: @L151470: .str L151470
+.orga 0x151478 :: @L151478: .str L151478
+.orga 0x151488 :: @L151488: .str L151488
+.orga 0x151498 :: @L151498: .str L151498
+.orga 0x1514A8 :: @L1514A8: .str L1514A8
+.orga 0x1514B8 :: @L1514B8: .str L1514B8
+.orga 0x1514C8 :: @L1514C8: .str L1514C8
+.orga 0x1514D8 :: @L1514D8: .str L1514D8
+.orga 0x1514E8 :: @L1514E8: .str L1514E8
+.orga 0x1514F8 :: @L1514F8: .str L1514F8
+.orga 0x151510 :: @L151510: .str L151510
+.orga 0x151528 :: @L151528: .str L151528
+.orga 0x151538 :: @L151538: .str L151538
+.orga 0x151548 :: @L151548: .str L151548
+.orga 0x151558 :: @L151558: .str L151558
+.orga 0x151568 :: @L151568: .str L151568
+.orga 0x151578 :: @L151578: .str L151578
+.orga 0x151580 :: @L151580: .str L151580
+.orga 0x151598 :: @L151598: .str L151598
+.orga 0x1515A8 :: @L1515A8: .str L1515A8
+.orga 0x1515B8 :: @L1515B8: .str L1515B8
+.orga 0x1515C8 :: @L1515C8: .str L1515C8
+.orga 0x1515D0 :: @L1515D0: .str L1515D0
+.orga 0x1515E8 :: @L1515E8: .str L1515E8
+.orga 0x1515F8 :: @L1515F8: .str L1515F8
+.orga 0x151608 :: @L151608: .str L151608
+.orga 0x151618 :: @L151618: .str L151618
+.orga 0x151628 :: @L151628: .str L151628
+.orga 0x151630 :: @L151630: .str L151630
+.orga 0x151640 :: @L151640: .str L151640
+.orga 0x151658 :: @L151658: .str L151658
+.orga 0x151670 :: @L151670: .str L151670
+.orga 0x151690 :: @L151690: .str L151690
+.orga 0x1516A0 :: @L1516A0: .str L1516A0
+.orga 0x1516B8 :: @L1516B8: .str L1516B8
+.orga 0x1516C0 :: @L1516C0: .str L1516C0
+.orga 0x1516E0 :: @L1516E0: .str L1516E0
+.orga 0x151700 :: @L151700: .str L151700
+.orga 0x151720 :: @L151720: .str L151720
+.orga 0x151740 :: @L151740: .str L151740
+.orga 0x151770 :: @L151770: .str L151770
+.orga 0x1517A0 :: @L1517A0: .str L1517A0
+.orga 0x1517D0 :: @L1517D0: .str L1517D0
+.orga 0x151800 :: @L151800: .str L151800
+.orga 0x151820 :: @L151820: .str L151820
+.orga 0x151840 :: @L151840: .str L151840
+.orga 0x151860 :: @L151860: .str L151860
+.orga 0x151880 :: @L151880: .str L151880
+.orga 0x1518B0 :: @L1518B0: .str L1518B0
+.orga 0x1518D0 :: @L1518D0: .str L1518D0
+.orga 0x151900 :: @L151900: .str L151900
+.orga 0x151920 :: @L151920: .str L151920
+.orga 0x151940 :: @L151940: .str L151940
+.orga 0x151960 :: @L151960: .str L151960
+.orga 0x151990 :: @L151990: .str L151990
+.orga 0x1519C0 :: @L1519C0: .str L1519C0
+.orga 0x1519E0 :: @L1519E0: .str L1519E0
+.orga 0x151A10 :: @L151A10: .str L151A10
+.orga 0x151A40 :: @L151A40: .str L151A40
+.orga 0x151A80 :: @L151A80: .str L151A80
+.orga 0x151AA0 :: @L151AA0: .str L151AA0
+.orga 0x151AD0 :: @L151AD0: .str L151AD0
+.orga 0x151AF0 :: @L151AF0: .str L151AF0
+.orga 0x151B10 :: @L151B10: .str L151B10
+.orga 0x151B20 :: @L151B20: .str L151B20
+.orga 0x151B50 :: @L151B50: .str L151B50
+.orga 0x151B70 :: @L151B70: .str L151B70
+
+.orga 0x15AE48 :: @L15AE48: .str L15AE48
+.orga 0x15AE50 :: @L15AE50: .str L15AE50
+.orga 0x15AE58 :: @L15AE58: .str L15AE58
+.orga 0x15AE60 :: @L15AE60: .str L15AE60
+.orga 0x15AE68 :: @L15AE68: .str L15AE68
+.orga 0x15AE70 :: @L15AE70: .str L15AE70
+.orga 0x15AE78 :: @L15AE78: .str L15AE78
+.orga 0x15AE80 :: @L15AE80: .str L15AE80
+.orga 0x15AE88 :: @L15AE88: .str L15AE88
+.orga 0x15AE90 :: @L15AE90: .str L15AE90
+.orga 0x15AE98 :: @L15AE98: .str L15AE98
+.orga 0x15AEA0 :: @L15AEA0: .str L15AEA0
+.orga 0x15AEA8 :: @L15AEA8: .str L15AEA8
+.orga 0x15EB20 :: @L15EB20: .str L15EB20
+.orga 0x15EB28 :: @L15EB28: .str L15EB28
+.orga 0x15EB30 :: @L15EB30: .str L15EB30
+.orga 0x15EB38 :: @L15EB38: .str L15EB38
+.orga 0x15EB40 :: @L15EB40: .str L15EB40
+.orga 0x15EB48 :: @L15EB48: .str L15EB48
+.orga 0x15EB50 :: @L15EB50: .str L15EB50
+.orga 0x15EB58 :: @L15EB58: .str L15EB58
+.orga 0x15EB60 :: @L15EB60: .str L15EB60
+.orga 0x15EB68 :: @L15EB68: .str L15EB68
+.orga 0x15EB78 :: @L15EB78: .str L15EB78
+.orga 0x15EB80 :: @L15EB80: .str L15EB80
+.orga 0x15EB88 :: @L15EB88: .str L15EB88
+.orga 0x15EB90 :: @L15EB90: .str L15EB90
+.orga 0x15EB98 :: @L15EB98: .str L15EB98
+.orga 0x15EBA0 :: @L15EBA0: .str L15EBA0
+.orga 0x15EBA8 :: @L15EBA8: .str L15EBA8
+.orga 0x15EBB0 :: @L15EBB0: .str L15EBB0
+.orga 0x15EBB8 :: @L15EBB8: .str L15EBB8
+.orga 0x15EBC0 :: @L15EBC0: .str L15EBC0
+.orga 0x15EBC8 :: @L15EBC8: .str L15EBC8
+.orga 0x15EBD0 :: @L15EBD0: .str L15EBD0
+.orga 0x15EBD8 :: @L15EBD8: .str L15EBD8
+.orga 0x15EBE0 :: @L15EBE0: .str L15EBE0
+.orga 0x15EBE8 :: @L15EBE8: .str L15EBE8
+.orga 0x15EBF0 :: @L15EBF0: .str L15EBF0
+.orga 0x15EBF8 :: @L15EBF8: .str L15EBF8
+.orga 0x15EC00 :: @L15EC00: .str L15EC00
+.orga 0x15EC08 :: @L15EC08: .str L15EC08
+.orga 0x15EC10 :: @L15EC10: .str L15EC10
+.orga 0x15EC20 :: @L15EC20: .str L15EC20
+.orga 0x15EC40 :: @L15EC40: .str L15EC40
+.orga 0x15EC60 :: @L15EC60: .str L15EC60
+.orga 0x15EC90 :: @L15EC90: .str L15EC90
+.orga 0x15ECB0 :: @L15ECB0: .str L15ECB0
+.orga 0x15ECD0 :: @L15ECD0: .str L15ECD0
+.orga 0x15ECE0 :: @L15ECE0: .str L15ECE0
+.orga 0x15ED00 :: @L15ED00: .str L15ED00
+.orga 0x15EEA0 :: @L15EEA0: .str L15EEA0
+.orga 0x15EEC0 :: @L15EEC0: .str L15EEC0
