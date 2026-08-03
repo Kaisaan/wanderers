e413373a6a8f25c8fcf69277b3250097c39ca8fe
Kaisaan <34224128+Kaisaan@users.noreply.github.com>
2026-08-03 00:55:22 -0400

    Fix tutorial textbox width

--- scripts/tasks/update_asm.py
+++ scripts/tasks/update_asm.py
@@ -1,4 +1,4 @@
-from libwanderers.text import fix_ascii
+from libwanderers.text import fix_ascii, COLOR_CODES
 
 def generate_asm(strings_filename, rows):
     """
@@ -17,6 +17,12 @@ def generate_asm(strings_filename, rows):
                 en = "0x" + en.lstrip("b\"\\x").rstrip("\"")
             else: 
                 en = "\"" + en.replace('"', '\\"') + "\""
+
+            for value, tag in COLOR_CODES.items():      # Only matches the lowercase tags (e.g. "#pi" and not "Pi")
+                en = en.replace(
+                    tag,
+                    f"\", 0xFF, 0xFC, 0x0{value}, \""
+                )
 
             label = row[3]
             label = "L" + label
